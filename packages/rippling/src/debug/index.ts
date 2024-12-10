8ac4d0bca1512ae1e208c12e051c3d1c5eac9671
e7h4n <perfectworks@gmail.com>
2024-12-10 20:43:23 +0800

    refactor(debug): fix typo

--- packages/rippling/src/debug/index.ts
+++ packages/rippling/src/debug/index.ts
@@ -1,3 +1,5 @@
 export { nestedAtomToString } from './util';
 export { createDebugStore } from './debug-store';
+export { consoleLoggingInterceptor } from './logging-inspector';
 export type { DebugStore } from '../../types/debug/debug-store';
+export type { StoreInterceptor } from '../../types/core/store';
